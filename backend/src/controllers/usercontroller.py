8c84d4c10453a044789888a4e079115e7a1a7f00
Basel-Aljazaeri <114690771+Basel-Aljazaeri@users.noreply.github.com>
2023-05-16 15:06:30 +0200

    modifed

--- backend/src/controllers/usercontroller.py
+++ backend/src/controllers/usercontroller.py
@@ -3,7 +3,6 @@ from src.util.dao import DAO
 import re
 # updated validator
 emailValidator = re.compile(r'^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]+$')
-
 class UserController(Controller):
     def __init__(self, dao: DAO):
         super().__init__(dao=dao)
